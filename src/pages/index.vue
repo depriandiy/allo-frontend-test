ef167a72c111b71708cd166d8190abcd7952c8dd
Depri Andiyanto <depriandianton22@gmail.com>
2026-09-05 20:40:20 +0700

    chore: setup Pinia and stabilize starter dependencies

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+
+  <v-container class="py-8">
+    <h1>SpaceX Rockets</h1>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,14 +5,18 @@
  */
 
 // Plugins
+import { createPinia } from 'pinia'
 import vuetify from './vuetify'
 import router from '../router'
 
 // Types
 import type { App } from 'vue'
 
+const pinia = createPinia()
+
 export function registerPlugins (app: App) {
   app
     .use(vuetify)
+    .use(pinia)
     .use(router)
 }
